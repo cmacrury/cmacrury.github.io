220df928fde6433dd4bb45559a821bd0bcc50a43
cmacrury <75337824+cmacrury@users.noreply.github.com>
2026-01-16 16:21:27 +0000

    Deploying to gh-pages from @ cmacrury/cmacrury.github.io@73d99b6594b36bcf3ffc4c4b4519e4adc7d0b640 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,7 +438,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV_Calum_MacRury.pdf", "_blank");
         },
       },{
         id: 'social-email',
